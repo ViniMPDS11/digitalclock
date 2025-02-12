9f690e8750f87a1785a3ab32aa25ead7ef746351
ViniMPDS11 <vinimpdevs@gmail.com>
2025-02-12 13:35:33 -0300

    Data adicionada

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-const agora = new Date();
+const hoursText = document.querySelector('.hoursText')
+const dayText = document.querySelector('.dayText')
+const mouthText = document.querySelector('.mouthText')
+const yearText = document.querySelector('.yearText')
+const secondsText = document.querySelector('.secondsText')
 
-let horas = agora.getHours();
-let minutos = agora.getMinutes();
-let segundos = agora.getSeconds();
+const now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let day = now.getDate();
+let mouth = now.getMonth() + 1;
+let year = now.getFullYear();
+let seconds = now.getSeconds();
 
 function updateHours() {
-    const agora = new Date();
-    horas = agora.getHours();
-    minutos = agora.getMinutes();
-    segundos = agora.getSeconds();
+    const now = new Date();
+    hours = now.getHours();
+    minutes = now.getMinutes();
+    day = now.getDate();
+    mouth = now.getMonth() + 1;
+    year = now.getFullYear();
+    seconds = now.getSeconds();
 
-    horas = String(horas).padStart(2, '0');
-    minutos = String(minutos).padStart(2, '0');
-    segundos = String(segundos).padStart(2, '0');
+
+    hours = String(hours).padStart(2, '0');
+    minutes = String(minutes).padStart(2, '0');
+    day = String(day).padStart(2, '0');
+    mouth = String(mouth).padStart(2, '0');
+    year = String(year).padStart(2, '0');
+    seconds = String(seconds).padStart(2, '0');
 }
 
-const hours = document.querySelector('.hours')
-
 updateHours()
-hours.textContent = `${horas}:${minutos}:${segundos}`
+hoursText.textContent = `${hours}:${minutes}`
+dayText.textContent = `${day}`
+mouthText.textContent = `${mouth}`
+yearText.textContent = `${year}`
+secondsText.textContent = `${seconds}`
 
 setInterval(() => {
     updateHours()
-    hours.textContent = `${horas}:${minutos}:${segundos}`
+    hoursText.textContent = `${hours}:${minutes}`
+    dayText.textContent = `${day}`
+    mouthText.textContent = `${mouth}`
+    yearText.textContent = `${year}`
+    secondsText.textContent = `${seconds}`
 }, 1000);
